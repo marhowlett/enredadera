378e06a81832b388f655081a0fbfbfb3a6a7562b
Jesus <jesus@esbrillante.mx>
2021-04-15 17:46:48 -0500

    Algunos ajustes

--- templates/acerca.php
+++ templates/acerca.php
@@ -2,93 +2,65 @@
     /* Template Name: Acerca */ 
     get_header();
 ?>
- <!--
-  <div class="men">
-<ul>
- <li><a class="active" href="#home">Home</a></li>
-  <li><a href="#news">Acerca de<br>
-La Enredadera</a></li>
-  <li><a href="#contact">Proyectos</a></li>
-  <li><a href="#about">Entre miradas</a></li>
-   <li><a href="#about">Blog</a></li>
-   <li><a href="#about">Contacto</a></li>
-   <li><a href="#about"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/youtube.png" alt="youtube"></a></li>
- 
-<li><a href="#about"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/facebook.png" alt="fb"></a></li>
-<li><a href="#about"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/twitter.png" alt="tw"></a></li>
-<li><a href="#about"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/soundcloud_logo.png" alt="sound"></a></li>
-</ul>
-</div>
-  -->
-   <section id="acerca" class="template-section contenedor">
-   
-       <div class="grid grid2">
-       
-       <div class="col col5 primero">
-          <div id="imagen" class="logo2">
-   <div id="info">
-    
-     <p id="descripcion">
-     <?php echo get_custom('laenredadera_texto_1'); ?>
-     </p>
-   </div>
- </div>
-        
-       </div>
-       
-       
-        
-       
-       
-       <div class="col col5">
-          <div id="imagen" class="logo2">
-   <div id="info">
-    
-     <p id="descripcion">
-     <?php echo get_custom('laenredadera_texto_2'); ?>
-     </p>
-   </div>
- </div>
-        
-       </div>
-       
-       
-       
-        </div>
-      <div class="grid grid2 dos">
-      
-      <div class="col">
-          <div id="imagen" class="logo3">
-   <div id="info">
-    
-     <p id="descripcion">
-     <?php echo get_custom('laenredadera_texto_3'); ?>
-     </p>
-   </div>
- </div>
-        
-       </div>
-       
-       
-        <div class="col">
-          <div id="imagen" class="logo4">
-   <div id="info">
-    
-     <p id="descripcion">
-     <?php echo get_custom('laenredadera_texto_4'); ?>     
-     </p>
-   </div>
- </div>
-        
-       </div>
-      
-       
-       </div>
-       
 
-       
-        
-       
+<section id="acerca" class="template-section contenedor">
+
+  <div class="grid grid2">
+
+    <div class="col col5 primero">
+      <div id="imagen" class="logo2">
+        <div id="info">
+
+          <p id="descripcion">
+            <?php echo get_custom('laenredadera_texto_1'); ?>
+          </p>
+        </div>
+      </div>
+
+    </div>
+
+    <div class="col col5">
+      <div id="imagen" class="logo2">
+        <div id="info">
+
+          <p id="descripcion">
+            <?php echo get_custom('laenredadera_texto_2'); ?>
+          </p>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+  <div class="grid grid2 dos">
+
+    <div class="col">
+      <div id="imagen" class="logo3">
+        <div id="info">
+
+          <p id="descripcion">
+            <?php echo get_custom('laenredadera_texto_3'); ?>
+          </p>
+        </div>
+      </div>
+
+    </div>
+
+
+    <div class="col">
+      <div id="imagen" class="logo4">
+        <div id="info">
+
+          <p id="descripcion">
+            <?php echo get_custom('laenredadera_texto_4'); ?>
+          </p>
+        </div>
+      </div>
+
+    </div>
+
+  </div>
+
 </section>
 
 <?php 
--- templates/contacto.php
+++ templates/contacto.php
@@ -2,49 +2,45 @@
     /* Template Name: Contacto */ 
     get_header();
     $featured_img_url = get_the_post_thumbnail_url(get_the_ID(),'full'); 
-
 ?>
 
-    <section id="contacto" class="template-section contenedor">
-    <div class="grid">  
-      <div class="col3" style="background-image: url('<?php echo $featured_img_url; ?>');">
-       &nbsp;&nbsp;
+<section id="contacto" class="template-section contenedor">
+    <div class="grid">
+        <div class="col3" style="background-image: url('<?php echo $featured_img_url; ?>');">
+            &nbsp;&nbsp;
         </div>
-        
         <div class="col4">
             <div class="men2">
-<ul>
- 
-   
- 
-<!--<li><a href="<?php echo get_custom('facebook'); ?>"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/facebook.png" alt="fb"></a></li>-->
+                <ul>
+                    <!--<li><a href="<?php echo get_custom('facebook'); ?>"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/facebook.png" alt="fb"></a></li>-->
 
-
-<li><a href="<?php echo get_custom('instagram'); ?>"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/IG.png" alt="instagram"></a></li>
-<li><a href="<?php echo get_custom('soundcloud'); ?>"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/soundcloud_logo.png" alt="sound"></a></li>
-<li><a href="<?php echo get_custom('twitter'); ?>"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/twitter.png" alt="tw"></a></li>
-<li><a href="<?php echo get_custom('youtube'); ?>"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/youtube.png" alt="youtube"></a></li>
-</ul>
-</div>
+                    <li><a href="<?php echo get_custom('instagram'); ?>"> <img
+                                src="<?php echo get_stylesheet_directory_uri(); ?>/img/IG.png" alt="instagram"></a></li>
+                    <li><a href="<?php echo get_custom('soundcloud'); ?>"> <img
+                                src="<?php echo get_stylesheet_directory_uri(); ?>/img/soundcloud_logo.png"
+                                alt="sound"></a></li>
+                    <li><a href="<?php echo get_custom('twitter'); ?>"> <img
+                                src="<?php echo get_stylesheet_directory_uri(); ?>/img/twitter.png" alt="tw"></a></li>
+                    <li><a href="<?php echo get_custom('youtube'); ?>"> <img
+                                src="<?php echo get_stylesheet_directory_uri(); ?>/img/youtube.png" alt="youtube"></a>
+                    </li>
+                </ul>
+            </div>
             <ul>
-  <li><a href="/" >Inicio</a></li>
-  <li><a class="cursiva" href="/acerca">laenredadera</a></li>
-  <li><a href="/proyectos">Proyectos</a></li>
-  <li><a href="/entre">Entre miradas</a></li>
-   <li><a href="/blog2">Blog</a></li>
-   <li><a href="/contacto" class="primero">Contacto</a></li>
-</ul>
-       <p>Escríbenos:<br>
-<span><a href="mailto:<?php echo get_custom('correo'); ?>" title="enviar mensaje"><?php echo get_custom('correo'); ?></a></span></p>
+                <li><a href="/">Inicio</a></li>
+                <li><a class="cursiva" href="/acerca">laenredadera</a></li>
+                <li><a href="/proyectos">Proyectos</a></li>
+                <li><a href="/entre">Entre miradas</a></li>
+                <li><a href="/blog2">Blog</a></li>
+                <li><a href="/contacto" class="primero">Contacto</a></li>
+            </ul>
+            <p>Escríbenos:<br>
+                <span><a href="mailto:<?php echo get_custom('correo'); ?>"
+                        title="enviar mensaje"><?php echo get_custom('correo'); ?></a></span></p>
         </div>
-        </div>
-        
-       
+    </div>
 </section>
-      
-   
- 
- 
+
 <?php 
     get_footer();
 ?>
