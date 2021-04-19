1d37f4f5fe9bc0bc9f716eb582def02f70e41ca7
EmyRub <esme.rubi@hotmail.com>
2021-04-19 13:04:18 -0500

    Finalización de limpieza en maquetado contacto.php

--- templates/contacto.php
+++ templates/contacto.php
@@ -5,85 +5,6 @@
 ?>
 
 <section id="contacto" class="template-section contenedor">
-    <div class="grid">
-        <div class="col3" style="background-image: url('<?php echo $featured_img_url; ?>');">
-            &nbsp;&nbsp;
-        </div>
-        <div class="col4">
-            <div class="men2">
-                <ul>
-                    <!--<li><a href="<?php echo get_custom('facebook'); ?>"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/facebook.png" alt="fb"></a></li>-->
-
-                    <li><a href="<?php echo get_custom('instagram'); ?>"> <img
-                                src="<?php echo get_stylesheet_directory_uri(); ?>/img/IG.png" alt="instagram"></a></li>
-                    <li><a href="<?php echo get_custom('soundcloud'); ?>"> <img
-                                src="<?php echo get_stylesheet_directory_uri(); ?>/img/soundcloud_logo.png"
-                                alt="sound"></a></li>
-                    <li><a href="<?php echo get_custom('twitter'); ?>"> <img
-                                src="<?php echo get_stylesheet_directory_uri(); ?>/img/twitter.png" alt="tw"></a></li>
-                    <li><a href="<?php echo get_custom('youtube'); ?>"> <img
-                                src="<?php echo get_stylesheet_directory_uri(); ?>/img/youtube.png" alt="youtube"></a>
-                    </li>
-                </ul>
-            </div>
-            <ul>
-                <li><a href="/">Inicio</a></li>
-                <li><a class="cursiva" href="/acerca">laenredadera</a></li>
-                <li><a href="/proyectos">Proyectos</a></li>
-                <li><a href="/entre">Entre miradas</a></li>
-                <li><a href="/blog2">Blog</a></li>
-                <li><a href="/contacto" class="primero">Contacto</a></li>
-            </ul>
-            <p>Escríbenos:<br>
-                <span><a href="mailto:<?php echo get_custom('correo'); ?>"
-                        title="enviar mensaje"><?php echo get_custom('correo'); ?></a></span></p>
-        </div>
-    </div>
-</section>
-
-<section id="contacto" class="template-section contenedor">
-    <div class="main-grid">
-
-        <div class="principal" style="background-image: url('<?php echo $featured_img_url; ?>');">
-            &nbsp;&nbsp;
-        </div>
-
-        <div class="menu">
-            
-            <div class="men2">            
-                <ul>
-                    <!--<li><a href="<?php echo get_custom('facebook'); ?>"> <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/facebook.png" alt="fb"></a></li>-->
-
-                    <li><a href="<?php echo get_custom('instagram'); ?>"> <img
-                                src="<?php echo get_stylesheet_directory_uri(); ?>/img/IG.png" alt="instagram"></a></li>
-                    <li><a href="<?php echo get_custom('soundcloud'); ?>"> <img
-                                src="<?php echo get_stylesheet_directory_uri(); ?>/img/soundcloud_logo.png"
-                                alt="sound"></a></li>
-                    <li><a href="<?php echo get_custom('twitter'); ?>"> <img
-                                src="<?php echo get_stylesheet_directory_uri(); ?>/img/twitter.png" alt="tw"></a></li>
-                    <li><a href="<?php echo get_custom('youtube'); ?>"> <img
-                                src="<?php echo get_stylesheet_directory_uri(); ?>/img/youtube.png" alt="youtube"></a>
-                    </li>
-                </ul>
-            </div>
-
-            <ul>
-                <li><a href="/">Inicio</a></li>
-                <li><a class="cursiva" href="/acerca">laenredadera</a></li>
-                <li><a href="/proyectos">Proyectos</a></li>
-                <li><a href="/entre">Entre miradas</a></li>
-                <li><a href="/blog2">Blog</a></li>
-                <li><a href="/contacto" class="primero">Contacto</a></li>
-            </ul>
-
-            <p>Escríbenos:<br>
-                <span><a href="mailto:<?php echo get_custom('correo'); ?>"
-                        title="enviar mensaje"><?php echo get_custom('correo'); ?></a></span></p>
-        </div>
-    </div>
-</section>
-
-<section id="contacto" class="template-section contenedor">
     <div class="main-grid">
 
         <div class="principal" style="background-image: url('<?php echo $featured_img_url; ?>');">
@@ -91,9 +12,7 @@
         </div>
 
         <div class="menu flex">
-            
             <div class="box">
-
                 <nav class="iconos">
                     <a href="<?php echo get_custom('instagram'); ?>"> 
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/IG.png" alt="instagram">
@@ -109,8 +28,7 @@
 
                     <a href="<?php echo get_custom('youtube'); ?>"> 
                         <img src="<?php echo get_stylesheet_directory_uri(); ?>/img/youtube.png" alt="youtube">
-                    </a>
-                        
+                    </a>                        
                 </nav>
            
                 <ul>
@@ -130,9 +48,10 @@
                     </span>
                 </p>
 
-            </div><!--flex!-->                
+            </div><!--box!-->                
         </div><!--menu!-->
-    </div>
+
+    </div><!--main-grid!-->
 </section>
 
 <?php 
