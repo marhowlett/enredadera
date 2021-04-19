318edac3fab8f06329650aecc2557dcd2115f046
EmyRub <esme.rubi@hotmail.com>
2021-04-19 14:09:42 -0500

    Se agregó la clase webp para el resto de las imágenes en main.less

--- templates/acerca.php
+++ templates/acerca.php
@@ -3,67 +3,6 @@
     get_header();
 ?>
 
-<!-- <section id="acerca" class="template-section contenedor">
-
-  <div class="grid grid2">
-
-    <div class="col col5 primero">
-      <div id="imagen" class="logo2">
-        <div id="info">
-
-          <p id="descripcion">
-            <?php echo get_custom('laenredadera_texto_1'); ?>
-          </p>
-        </div>
-      </div>
-
-    </div>
-
-    <div class="col col5">
-      <div id="imagen" class="logo2">
-        <div id="info">
-
-          <p id="descripcion">
-            <?php echo get_custom('laenredadera_texto_2'); ?>
-          </p>
-        </div>
-      </div>
-
-    </div>
-
-  </div>
-  
-  <div class="grid grid2 dos">
-
-    <div class="col">
-      <div id="imagen" class="logo3">
-        <div id="info">
-
-          <p id="descripcion">
-            <?php echo get_custom('laenredadera_texto_3'); ?>
-          </p>
-        </div>
-      </div>
-
-    </div>
-
-
-    <div class="col">
-      <div id="imagen" class="logo4">
-        <div id="info">
-
-          <p id="descripcion">
-            <?php echo get_custom('laenredadera_texto_4'); ?>
-          </p>
-        </div>
-      </div>
-
-    </div>
-
-  </div>
-
-</section> -->
-
 <section id="acerca" class="template-section contenedor">
 
   <div class="cuadricula two">    
--- templates/inicio.php
+++ templates/inicio.php
@@ -2,64 +2,6 @@
     /* Template Name: Inicio */ 
     get_header();
 ?>
-<!-- <section id="ini" class="template-section">
-
-    <div class="grid" id="mosaico">
-
-        <div class="col3" id="getid">
-
-            <div class="grid grid2">
-                <div data-celda="1" class="celda celda_1 col2 hvr-wobble-to-bottom-right altura">
-                    &nbsp;&nbsp;
-                </div>
-                <div data-celda="2" class="celda celda_2 col2 hvr-wobble-to-bottom-right altura">
-                    &nbsp;&nbsp;
-                </div>
-                <div data-celda="3" class="celda celda_3 col2 hvr-wobble-to-bottom-right altura">
-                    &nbsp;&nbsp;
-                </div>
-
-            </div>
-            <div class="grid grid2">
-                <div data-celda="4" class="celda celda_4 col2 hvr-wobble-to-bottom-right altura2">
-                    &nbsp;&nbsp;
-                </div>
-                <div data-celda="5" class="celda celda_5 col2 hvr-wobble-to-bottom-right altura2">
-                    &nbsp;&nbsp;
-                </div>
-                <div data-celda="6" class="celda celda_6 col2 hvr-wobble-to-bottom-right altura2">
-                    &nbsp;&nbsp;
-                </div>
-            </div>
-
-            <div class="grid grid2">
-                <div data-celda="7" class="celda celda_7 col2 hvr-wobble-to-bottom-right altura3">
-                    &nbsp;&nbsp;
-                </div>
-
-                <div data-celda="8" class="celda celda_8 col2 hvr-wobble-to-bottom-right altura3">
-                    &nbsp;&nbsp;
-                </div>
-                <div data-celda="9" class="celda celda_9 col2 hvr-wobble-to-bottom-right altura3">
-                    &nbsp;&nbsp;
-                </div>
-            </div>
-        </div>
-
-        <div class="col4">
-            <p>www.laenredadera.com</p>
-            <ul>
-                <li><a href="/" class="primero">Inicio</a></li>
-                <li><a href="/acerca" class="cursiva ">laenredadera</a></li>
-                <li><a href="/proyectos">Proyectos</a></li>
-                <li><a href="/entre">Entre miradas</a></li>
-                <li><a href="/blog2">Blog</a></li>
-                <li><a href="/contacto">Contacto</a></li>
-            </ul>
-        </div>
-    </div>
-
-</section> -->
 
 <section id="ini" class="template-section">
 
@@ -120,7 +62,6 @@
     </div>
 
 </section>
-
 
 <?php 
     get_footer();
